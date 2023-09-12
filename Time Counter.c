f82f2d3fc005869e11ec84a3aa1fafffd5cbb26b
Benjie <141549818+XiaPlayz@users.noreply.github.com>
2023-09-12 21:24:01 +0800

    If user input is not number it will automatically print Invalid Numbers

--- Time Counter.c
+++ Time Counter.c
@@ -1,13 +1,20 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <unistd.h>
+
 int main() {
     int count;
     printf("Let's Count From one to?: ");
-    scanf("%d", &count);
+    if (scanf("%d", &count) != 1) {
+        printf("Invalid number\n");
+        return 1;//If user input is not number it will automatically print Invalid Numbers
+    }
 
     for (int i = 1; i <= count; i++) {
         printf("The value of count is %d\n", i);
-        
+
         sleep(1);
     }
-    
+
     return 0;
 }
